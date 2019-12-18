93a9ad327180b331ac23826562d35223f6ab86b0
sevencrime <onedi@qq.com>
2019-12-18 15:27:34 +0800

    快讯-期货

--- News.py
+++ News.py
@@ -10,62 +10,49 @@ from browsermobproxy import Server
 from selenium.webdriver.chrome.options import Options
 import requests
 import re
+from BasePage import BasePage
+import datetime
 
-class News():
+
+class News(BasePage):
 
     def __init__(self, url):
         self.url = url
-        self.mobproxybat = "D:/下载/browsermob-proxy-2.1.4/bin/browsermob-proxy.bat"
-        # self.mobproxybat = "C:/Program Files (x86)/Google/Chrome/browsermob-proxy-2.1.4/bin/browsermob-proxy.bat"
-
-    def find_element(self, driver, *loc):
-        try:
-            # 确保元素是可见的。
-            # 注意：以下入参为元组的元素，需要加*。Python存在这种特性，就是将入参放在元组里。
-            WebDriverWait(self.driver,10).until(lambda driver: driver.find_element(*loc).is_displayed())
-            # 注意：以下入参本身是元组，不需要加*
-            WebDriverWait(self.driver, 10).until(
-                EC.element_to_be_clickable(loc))
-            
-            # WebDriverWait(self.driver, 20).until(
-            #     EC.visibility_of_element_located(loc))
-
-            return self.driver.find_element(*loc)
-        except:
-            print(u"%s 页面中未能找到 %s 元素" % (self, loc))
-            flag = False
-            return flag
 
     def same_flashData(self, flashdata, flashaip):
         diff_list = []
+        print("***************************************")    
         for d in flashdata:
             try:
                 assert d in flashaip
             except AssertionError:
+                print("不一致的值为: {}".format(d))
                 diff_list.append(d)
                 continue
 
         return diff_list
 
-    def get_flashAPI(self):
+    def get_flashAPI(self, max_time=None):
         url = "http://114.55.249.227:8080/eddid/flash_list"
         api_flashList = []
         data = {
-            'channel' : -8200
+            'channel' : -8200, 
         }
+        if max_time != None:
+            data['max_time'] = max_time
 
         resp = requests.get(url, params=data).json()
-
+        print(type(resp))
+        # import pdb; pdb.set_trace()
         for res in resp['data']:
             # print(res['data'])
             if 'content' in res['data'].keys():
 
-                if res['data']['content'].find("<b>") != -1:
-                    c_text = ''.join(re.findall(r"<b>(.+)</b>", res['data']['content']))
-                    api_flashList.append(c_text)
-                    continue
+                reg = re.compile('<[^>]*>')
+                content = reg.sub('',res['data']['content']).replace('\n','')
+                # print(content)
+                api_flashList.append(content)
 
-                api_flashList.append(res['data']['content'])
 
             elif [k in ['name', 'country', 'time_period'] for k in resp.keys()]:
                 # res['data']['country']
@@ -73,80 +60,77 @@ class News():
                 # res['data']['name']
                 api_flashList.append(res['data']['country'] + res['data']['time_period'] + res['data']['name'])
 
-        print("**********************************************************")
-        # print(api_flashList)
-        return api_flashList    
+        # print("**********************************************************")
+        print("接口返回数据的条数为 : {}".format(len(api_flashList)))
+        return api_flashList, resp['data'][-1]
 
-
-    def get_flash_futures(self):
-        print("开始请求资源网站")
-        # 建立browsermobproxy服务, 需指定browsermob-proxy, 类似chromedriver
-        server = Server(self.mobproxybat)
-        server.start()
-        # 创建代理
-        proxy = server.create_proxy()
-        chrome_options = Options()
-        # 为chrome启动时设置代理
-        chrome_options.add_argument('--proxy-server={0}'.format(proxy.proxy))
-        # 静默模式, 不显示浏览器
-        chrome_options.add_argument('headless')
-
-        driver = webdriver.Chrome(
-            executable_path='C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe',
-            chrome_options=chrome_options)
-
-        # driver.set_script_timeout(3)
-
-        # 这设置了要记录的新HAR(HTTP Archive format(HAR文件)，是用来记录浏览器加载网页时所消耗的时间的工具)
-        proxy.new_har(ref="HAR啦", options={'captureHeaders': True, 'captureContent': True}, title="标题")
-        driver.get(self.url)
-
-        WebDriverWait(driver, 20).until_not(
-            EC.presence_of_element_located((By.XPATH, '//div[contains(text(), "暂无数据")]')))
-
-        # self.find_element(driver, (By.XPATH, '//button'))
-
-        # 获取HAR
-        # result = proxy.har
-        # print(result)
-        # print(driver.page_source)
-
+    def lxml_parse(self, driver):
         soup = BeautifulSoup(driver.page_source,'lxml')
         context = soup.select("div.md-example-child.news p.md-cell-item-title")
         spantitle = soup.select("div.item-one.item-two > span.title")
-
         flashList = []
-
         for text in context:
             # print(text.get_text())
             flashList.append(text.get_text())
 
         for span in spantitle:
             # print(span.get_text())
-            flashList.append(span.get_text())
-
-
-
-        # 代理需要关闭
-        print("已经要关闭啦!!!!")
-        server.stop()
-        driver.quit()
-
-        # print(flashList)
-
+            flashList.append(span.get_text())       
         return flashList
+
+    def get_flash_futures(self):
+        print("开始请求资源网站")
+        chrome_options = Options()
+        # 静默模式, 不显示浏览器
+        # chrome_options.add_argument('headless')
+
+        driver = webdriver.Chrome(
+            executable_path='C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe',
+            chrome_options=chrome_options)
+
+        super(News, self).__init__(driver, self.url)
+        # 打开浏览器
+        self.open()
+
+        # 等待数据加载
+        WebDriverWait(driver, 20).until_not(
+            EC.presence_of_element_located((By.XPATH, '//div[contains(text(), "暂无数据")]')))
+
+        # print(driver.page_source)
+
+        flashList = self.lxml_parse(driver)
+
+        add_btn = driver.find_element_by_xpath('//button')
+        # 点击加载更多按钮
+        self.scrollinto(add_btn)
+        # self.script("arguments[0].scrollIntoView();", add_btn)
+
+        loading = (By.XPATH, '//div[@class="md-popup-box md-fade" and @style="display: none;"]')
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(loading))
+
+        addflashList = self.lxml_parse(driver)
+
+        print("已经要关闭啦!!!!")
+        driver.quit()
+        
+        # import pdb; pdb.set_trace
+        print("flash_list页面返回的条数为 : {}".format(len(flashList)))
+        return flashList, addflashList
 
 def test_News_futures():
     url = 'https://download.eddidapp.com/page/eddid-news/index.html'
     n = News(url)   
-    flashList = n.get_flash_futures()
-    flash_api = n.get_flashAPI()
+    flashList, addflashList= n.get_flash_futures()
+    flash_api, lastdata = n.get_flashAPI()
     diff_list = n.same_flashData(flashList, flash_api)
     # print("对比不一致的内容有: ", diff_list)
-    for diff in diff_list:
-        print("对比不一致的内容有: ", diff)
-
-
+    print("********************************")
+    strtime = datetime.datetime.strptime(lastdata['time'], "%Y-%m-%d %H:%M:%S")
+    startTime = (strtime - datetime.timedelta(seconds=1)).strftime("%Y-%m-%d %H:%M:%S")
+    print(startTime, "2222222222222222222222222222")
+    newflash_api, new_lastdata = n.get_flashAPI(startTime)
+    diff_list = n.same_flashData(addflashList, flash_api+newflash_api)
 
 if __name__ == '__main__':
     test_News_futures()
