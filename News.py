133e489a8e1ab2a3af69cdc5e148449c9e866c5f
sevencrime <onedi@qq.com>
2019-12-19 13:11:37 +0800

    修改api, 返回lastdata

--- News.py
+++ News.py
@@ -20,8 +20,8 @@ class News(BasePage):
         self.url = url
 
     def same_flashData(self, flashdata, flashaip):
+        print("正在对比数据")    
         diff_list = []
-        print("***************************************")    
         for d in flashdata:
             try:
                 assert d in flashaip
@@ -30,7 +30,29 @@ class News(BasePage):
                 diff_list.append(d)
                 continue
 
+        if diff_list == []:
+            print("数据一致")
         return diff_list
+
+    def driver(self):
+        chrome_options = Options()
+        # 静默模式, 不显示浏览器
+        chrome_options.add_argument('headless')
+
+        driver = webdriver.Chrome(
+            executable_path='C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe',
+            chrome_options=chrome_options)
+
+        super(News, self).__init__(driver, self.url)
+        # 打开浏览器
+        self.open()
+
+        # 等待数据加载
+        WebDriverWait(driver, 20).until_not(
+            EC.presence_of_element_located((By.XPATH, '//div[contains(text(), "暂无数据")]')))
+
+        return driver
+
 
     def get_flashAPI(self, max_time=None):
         url = "http://114.55.249.227:8080/eddid/flash_list"
@@ -41,8 +63,9 @@ class News(BasePage):
         if max_time != None:
             data['max_time'] = max_time
 
+        print("正在请求flash_list接口")
         resp = requests.get(url, params=data).json()
-        print(type(resp))
+        # print(type(resp))
         # import pdb; pdb.set_trace()
         for res in resp['data']:
             # print(res['data'])
@@ -62,9 +85,12 @@ class News(BasePage):
 
         # print("**********************************************************")
         print("接口返回数据的条数为 : {}".format(len(api_flashList)))
-        return api_flashList, resp['data'][-1]
+        strtime = datetime.datetime.strptime(resp['data'][-1]['time'], "%Y-%m-%d %H:%M:%S")
+        startTime = (strtime - datetime.timedelta(seconds=1)).strftime("%Y-%m-%d %H:%M:%S")
+        return api_flashList, startTime
 
     def lxml_parse(self, driver):
+        print("正在解析网页数据")
         soup = BeautifulSoup(driver.page_source,'lxml')
         context = soup.select("div.md-example-child.news p.md-cell-item-title")
         spantitle = soup.select("div.item-one.item-two > span.title")
@@ -80,28 +106,14 @@ class News(BasePage):
 
     def get_flash_futures(self):
         print("开始请求资源网站")
-        chrome_options = Options()
-        # 静默模式, 不显示浏览器
-        # chrome_options.add_argument('headless')
-
-        driver = webdriver.Chrome(
-            executable_path='C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe',
-            chrome_options=chrome_options)
-
-        super(News, self).__init__(driver, self.url)
-        # 打开浏览器
-        self.open()
-
-        # 等待数据加载
-        WebDriverWait(driver, 20).until_not(
-            EC.presence_of_element_located((By.XPATH, '//div[contains(text(), "暂无数据")]')))
-
+        driver = self.driver()
         # print(driver.page_source)
 
         flashList = self.lxml_parse(driver)
 
         add_btn = driver.find_element_by_xpath('//button')
         # 点击加载更多按钮
+        print("点击加载更多按钮")
         self.scrollinto(add_btn)
         # self.script("arguments[0].scrollIntoView();", add_btn)
 
@@ -121,15 +133,15 @@ class News(BasePage):
 def test_News_futures():
     url = 'https://download.eddidapp.com/page/eddid-news/index.html'
     n = News(url)   
+    print("开始爬取快讯-期货的数据")
     flashList, addflashList= n.get_flash_futures()
     flash_api, lastdata = n.get_flashAPI()
     diff_list = n.same_flashData(flashList, flash_api)
     # print("对比不一致的内容有: ", diff_list)
     print("********************************")
-    strtime = datetime.datetime.strptime(lastdata['time'], "%Y-%m-%d %H:%M:%S")
-    startTime = (strtime - datetime.timedelta(seconds=1)).strftime("%Y-%m-%d %H:%M:%S")
-    print(startTime, "2222222222222222222222222222")
-    newflash_api, new_lastdata = n.get_flashAPI(startTime)
+    # strtime = datetime.datetime.strptime(lastdata['time'], "%Y-%m-%d %H:%M:%S")
+    # startTime = (strtime - datetime.timedelta(seconds=1)).strftime("%Y-%m-%d %H:%M:%S")
+    newflash_api, lastdata = n.get_flashAPI(lastdata)
     diff_list = n.same_flashData(addflashList, flash_api+newflash_api)
 
 if __name__ == '__main__':
