f1128aff57433357f00e9abc912c298e9db961a9
sevencrime <onedi@qq.com>
2020-01-22 13:01:52 +0800

    加个逗号

--- src/test_News.py
+++ src/test_News.py
@@ -378,7 +378,7 @@ def run():
     gm.set_List("errfunc", [])
     gm.set_value(nowtime=datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'))
 
-    pytest.main(["-s", "-v", "test_News.py" 
+    pytest.main(["-s", "-v", "test_News.py",
                  "--alluredir",
                  rootPath + '/report/xml_{time}'.format(time=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')),
                  "--reruns=2",  # pip install pytest-rerunfailures
